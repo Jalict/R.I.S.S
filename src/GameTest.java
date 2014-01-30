2690c9c1272bc939db033ba0b8b866be078f7dcc
Frans Peter Larsen <larsen.frans@gmail.com>
2014-01-30 01:44:45 +0100

    Added Movement
    Made so it possible to move the var via WASD (Albeit no S).

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,6 +1,9 @@
+import java.awt.Point;
 import java.awt.image.PixelGrabber;
+import java.util.Vector;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Vector2f;
  
 public class GameTest extends BasicGame {
 	private Image imageBackground;
@@ -10,7 +13,8 @@ public class GameTest extends BasicGame {
 	/*private int bestTime;
 	private int currentTime;*/
 	private Image test;
-	private int px,py;
+	private Vector2f p;
+	private float angle;
 	
 	public GameTest(String title) { 
 		super(title); 
@@ -18,6 +22,7 @@ public class GameTest extends BasicGame {
  
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new GameTest("Race"));
+		app.setShowFPS(false);
 		app.start();
 	}
 	
@@ -25,35 +30,51 @@ public class GameTest extends BasicGame {
 	public void init(GameContainer gc) throws SlickException {
 		imageBackground = new Image("texture/level/1.png");
 		imageCollsionMap = new Image("texture/level/1c.png");
-		test = new Image("texture/test.png");
+		test = new Image("texture/test.png");		
+		p = new Vector2f(163,309);
+		angle = 6;
 	}
  
 	@Override
 	public void render(GameContainer gc, Graphics g)	throws SlickException {
 		imageBackground.draw();
 		
-		if(imageCollsionMap.getColor(px, py).equals(new Color(255, 0, 0))) {
-			test.draw(16, 16);
-		}
+		test.draw(p.getX(), p.getY());
+		
 		g.drawString("lap: "+lap, 16, 32);
-		g.drawString("cp: "+checkpoint, 16, 48);
 	}
  
 	@Override
-	public void update(GameContainer gc, int delta) throws SlickException {
-		px = gc.getInput().getAbsoluteMouseX();
-		py = gc.getInput().getAbsoluteMouseY();
+	public void update(GameContainer gc, int dt) throws SlickException {
+		Input in = gc.getInput();
 		
-		if(imageCollsionMap.getColor(px, py).equals(new Color(0, 255, 0))) {
+		if(in.isKeyDown(in.KEY_W)) {
+			p.x += (Math.sin(angle)/8)*dt;
+		    p.y += (Math.cos(angle)/8)*dt;
+		}
+		
+		if(in.isKeyDown(in.KEY_A)) {
+			angle += 0.005;
+		}
+		if(in.isKeyDown(in.KEY_D)) {
+			angle -= 0.005;
+		}
+		
+		if(imageCollsionMap.getColor((int)p.getX(), (int)p.getY()).equals(new Color(0, 255, 0))) {
 			checkpoint = true;
 		}
 		
-		if(imageCollsionMap.getColor(px, py).equals(new Color(0, 0, 255)) && checkpoint) {
+		if(imageCollsionMap.getColor((int)p.getX(), (int)p.getY()).equals(new Color(0, 0, 255)) && checkpoint) {
 			checkpoint = false;
 			lap++;
+			//TODO: Get current time
 			/*if(currentTime>bestTime) {
 				bestTime = currentTime;
 			}*/
+		}
+		
+		if(imageCollsionMap.getColor((int)p.getX(), (int)p.getY()).equals(new Color(255, 0, 0))) {
+			//TODO: Make velocity slower in case of being outside track
 		}
 	}
 }
